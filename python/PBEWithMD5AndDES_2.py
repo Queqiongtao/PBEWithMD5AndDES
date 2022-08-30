f1f3a4347867492cc1e8f8d5f544aa2faab298bc
Queqiongtao <467500670@qq.com>
2022-08-30 17:34:35 +0800

    Update PBEWithMD5AndDES_2.py
    更改适应python3.7  加入中文提示操作

--- python/PBEWithMD5AndDES_2.py
+++ python/PBEWithMD5AndDES_2.py
@@ -6,7 +6,6 @@ from Crypto.Cipher import DES
 
 """
 Note about PBEWithMD5AndDES in java crypto library:
-
 Encrypt:
   Generate a salt (random): 8 bytes
   <start derived key generation>
@@ -18,25 +17,22 @@ Encrypt:
   the least significant bit needs to be changed to 1 if required. We don't do it,
   maybe the python crypto library does it for us)
   <end derived key generation>
-
   Pad the input string with 1-8 bytes (note: not 0-7, so we always have padding)
     so that the result is a multiple of 8 bytes. Padding byte value is same as number of
     bytes being padded, eg, \x07 if 7 bytes need to be padded.
   Use the key and iv to encrypt the input string, using DES with CBC mode.
   Prepend the encrypted value with the salt (needed for decrypting since it is random)
   Base64 encode it -> this is your result
-
 Decrypt:
   Base64 decode the input message
   Extract the salt (first 8 bytes). The rest is the encoded text.
   Use derived key generation as in Encrypt above to get the key and iv
   Decrypt the encoded text using key and iv
   Remove padding -> this is your result
-
 """
 
 def get_derived_key(password, salt, count):
-    key = password + salt
+    key = bytes(password, encoding="UTF-8") + bytes(salt)
     for i in range(count):
         m = hashlib.md5(key)
         key = m.digest()
@@ -50,7 +46,8 @@ def decrypt(msg, password):
     crypter = DES.new(dk, DES.MODE_CBC, iv)
     text = crypter.decrypt(enc_text)
     # remove the padding at the end, if any
-    return re.sub(r'[\x01-\x08]','',text)
+    print(text.decode("UTF-8"))
+    return re.sub(r'[\x01-\x08]','',text.decode("UTF-8"))
 
 def encrypt(msg, password):
     salt = os.urandom(8)
@@ -59,16 +56,28 @@ def encrypt(msg, password):
         msg += chr(pad_num)
     (dk, iv) = get_derived_key(password, salt, 1000)
     crypter = DES.new(dk, DES.MODE_CBC, iv)
-    enc_text = crypter.encrypt(msg)
+
+    enc_text = crypter.encrypt(msg.encode('UTF-8'))
     return base64.b64encode(salt + enc_text)
 
 def main():
-    msg = "hello, world"
-    passwd = "mypassword"
-    s = encrypt(msg, passwd)
-    print s
-    print decrypt(s, passwd)
+
+
+    msg = ""
+    passwd = ""
+
+    passwd += input("输入口令：")
+    print("你输入的口令是：%s" % passwd)
+    par_type = input("输入D：解密  E:加密 其他：退出")
+    if (par_type == "D"):
+        msg = input("请输入密文：")
+        print(decrypt(msg, passwd))
+
+    elif (par_type == "E"):
+        re.sub(r'[\x01-\x08]', '', encrypt(msg, passwd).decode("UTF-8"))
+
+
+
 
 if __name__ == "__main__":
     main()
-
